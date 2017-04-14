a7862bb7100c15f5953800b3546334fbda5034e3
lawrence <402374437@qq.com>
2017-04-15 00:44:15 +0800

    default

---
+++ src/main/java/com/hrbeu/zzw/SmarttravelApplication.java
@@ -0,0 +1,12 @@
+package com.hrbeu.zzw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarttravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarttravelApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hrbeu/zzw/model/User.java
@@ -0,0 +1,7 @@
+package com.hrbeu.zzw.model;
+
+/**
+ * Created by lawrence on 2017/4/15.
+ */
+public class User {
+}
